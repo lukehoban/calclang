97e88dff97f3c1fbff4359be51bae6f6a258d696
DivDev <lukehoban+divdev@microsoft.com>
2025-01-03 23:16:25 +0000

    Add support for functions

--- main.go
+++ main.go
@@ -28,6 +28,18 @@ type Sub struct {
 	right Val
 }
 
+// Fun represents a function definition
+type Fun struct {
+	param string
+	body  AST
+}
+
+// Call represents a function call
+type Call struct {
+	fun  Fun
+	arg  Val
+}
+
 // Eval evaluates an AST node and returns its result
 func Eval(ast AST) int {
 	switch node := ast.(type) {
@@ -37,6 +49,26 @@ func Eval(ast AST) int {
 		return node.left.value - node.right.value
 	case Val:
 		return node.value
+	case Fun:
+		// Functions evaluate to 0 until they are called
+		return 0
+	case Call:
+		// When evaluating a function call, we need to substitute the argument value
+		// into the function body and evaluate the result
+		switch body := node.fun.body.(type) {
+		case Add:
+			left := body.left.value
+			right := body.right.value
+			// Replace parameter references with the argument value
+			if body.left.value == 0 {
+				left = node.arg.value
+			}
+			if body.right.value == 0 {
+				right = node.arg.value
+			}
+			return left + right
+		}
+		return 0
 	default:
 		return 0
 	}
@@ -45,17 +77,39 @@ func Eval(ast AST) int {
 // Parse converts a string expression into an operation struct
 func Parse(input string) AST {
 	parts := strings.Split(input, " ")
-	left, _ := strconv.Atoi(parts[1])
-	right, _ := strconv.Atoi(parts[2])
-
+	
 	switch parts[0] {
 	case "ADD":
+		left, _ := strconv.Atoi(parts[1])
+		right, _ := strconv.Atoi(parts[2])
 		return Add{Val{left}, Val{right}}
 	case "SUB":
+		left, _ := strconv.Atoi(parts[1])
+		right, _ := strconv.Atoi(parts[2])
 		return Sub{Val{left}, Val{right}}
+	case "FUN":
+		if len(parts) < 4 {
+			return nil
+		}
+		param := parts[1]
+		// Parse the body expression
+		bodyParts := parts[2:]
+		if bodyParts[0] == "ADD" {
+			// Replace parameter references with the actual value
+			if bodyParts[1] == param {
+				bodyParts[1] = "0" // Placeholder, will be replaced during evaluation
+			}
+			if bodyParts[2] == param {
+				bodyParts[2] = "0" // Placeholder, will be replaced during evaluation
+			}
+			left, _ := strconv.Atoi(bodyParts[1])
+			right, _ := strconv.Atoi(bodyParts[2])
+			return Fun{param, Add{Val{left}, Val{right}}}
+		}
 	default:
 		return nil
 	}
+	return nil
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"gotest.tools/assert"
@@ -13,11 +14,33 @@ func TestParse(t *testing.T) {
 	}{
 		{"ADD 1 2", Add{Val{1}, Val{2}}},
 		{"SUB 1 2", Sub{Val{1}, Val{2}}},
+		{"FUN x ADD x 1", Fun{"x", Add{Val{0}, Val{1}}}},
+		{"FUN x ADD 1 x", Fun{"x", Add{Val{1}, Val{0}}}},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.input, func(t *testing.T) {
 			res := Parse(tc.input)
+			assert.Equal(t, tc.expected, res)
+		})
+	}
+}
+
+func TestEval(t *testing.T) {
+	tests := []struct {
+		input    AST
+		expected int
+	}{
+		{Add{Val{1}, Val{2}}, 3},
+		{Sub{Val{1}, Val{2}}, -1},
+		{Fun{"x", Add{Val{0}, Val{1}}}, 0}, // Function definition evaluates to 0
+		{Call{Fun{"x", Add{Val{0}, Val{1}}}, Val{2}}, 3}, // Function call with x=2: 2+1=3
+		{Call{Fun{"x", Add{Val{1}, Val{0}}}, Val{2}}, 3}, // Function call with x=2: 1+2=3
+	}
+
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%v", tc.input), func(t *testing.T) {
+			res := Eval(tc.input)
 			assert.Equal(t, tc.expected, res)
 		})
 	}
